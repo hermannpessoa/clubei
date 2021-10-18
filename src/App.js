4679123d7d48e6ecd37cb727ffff1843d68785ad
Hermann Pavão <you@example.comhermann.pessoa@gmail.com>
2021-10-18 08:46:50 -0300

    json server ajuste

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './Styles/Main.scss';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
+// REF.
+// https://www.figma.com/file/z6xz8fkVzn0hXD5TfWUuYy/Prot%C3%B3tipo?node-id=0%3A1
+
 function App() {
   return (
    <BrowserRouter>
